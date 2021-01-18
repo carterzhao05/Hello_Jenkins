7e08978f367fc26135af999fb2568a3606fd5d24
carterzhao05 <carterzhao05@gmail.com>
2021-01-18 07:01:08 +0000

    cov

--- original/draw_cov.sh
+++ original/draw_cov.sh
@@ -4,7 +4,8 @@ fullPathTarget=hello_fuzz
 target=$(basename $fullPathTarget )
 mkdir -p cov_viz
 mkdir -p corpus/${target}_corpus/ corpus/${target}_seeds/
-LLVM_PROFILE_FILE="cov_viz/${target}.profraw"  ./${fullPathTarget}  corpus/${target}_corpus/ corpus/${target}_seeds/  -max_total_time=10
-llvm-profdata merge -sparse  cov_viz/${target}.profraw -o cov_viz/${target}.profdata
-llvm-cov show --format=html --output-dir=./cov_viz  ./${fullPathTarget} -instr-profile=cov_viz/${target}.profdata
-llvm-cov export -format=text  ./${fullPathTarget} -instr-profile=cov_viz/${target}.profdata > cov_viz/cov.json
+LLVM_PROFILE_FILE="${target}.profraw"  ./${fullPathTarget}  corpus/${target}_corpus/ corpus/${target}_seeds/  -max_total_time=10
+llvm-profdata merge -sparse  ${target}.profraw -o ${target}.profdata
+llvm-cov show --format=html --output-dir=.  ./${fullPathTarget} -instr-profile=${target}.profdata
+llvm-cov export -instr-profile ${target}.profdata ./${fullPathTarget}
+llvm-cov export -instr-profile ${target}.profdata ./${fullPathTarget} > cov.json
