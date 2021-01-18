1d8c67b5f9ed79f53a8849ae79b52763bef0304d
carterzhao05 <carterzhao05@gmail.com>
2021-01-18 09:45:19 +0000

    ok

--- hello_fuzz.cpp
+++ hello_fuzz.cpp
@@ -12,6 +12,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     while ( dataProvider.remaining_bytes() > 0 ) {
         int a = dataProvider.ConsumeIntegral<int>();
         int b = dataProvider.ConsumeIntegral<int>();
+        char ab[2];
+        ab[3]=1;
+        std::cout<<ab[4]<<"\n";
         if(a>5) return 0;
         add(a , b);
     }
