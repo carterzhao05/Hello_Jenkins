18f4a6293b47271908f5dbe5abcda3992cfca1e8
junzhao3 <junzhao3@cisco.com>
2021-01-18 06:24:40 +0000

    llvm cov

--- original/draw_cov.sh
+++ original/draw_cov.sh
@@ -1,9 +1,9 @@
-export PATH=/Users/carter/Downloads/third_party/llvm-build/Release+Asserts/bin:$PATH
+
 fullPathTarget=hello_fuzz
 target=$(basename $fullPathTarget )
+mkdir -p cov_viz
 mkdir -p corpus/${target}_corpus/ corpus/${target}_seeds/
 LLVM_PROFILE_FILE="cov_viz/${target}.profraw"  ./${fullPathTarget}  corpus/${target}_corpus/ corpus/${target}_seeds/  -max_total_time=10
 llvm-profdata merge -sparse  cov_viz/${target}.profraw -o cov_viz/${target}.profdata
 llvm-cov show --format=html --output-dir=./cov_viz  ./${fullPathTarget} -instr-profile=cov_viz/${target}.profdata
-llvm-cov show --format=text --output-dir=./cov_viz  ./${fullPathTarget} -instr-profile=cov_viz/${target}.profdata
-llvm-cov show --format=lcov --output-dir=./cov_viz  ./${fullPathTarget} -instr-profile=cov_viz/${target}.profdata
+llvm-cov export -format=text  ./${fullPathTarget} -instr-profile=cov_viz/${target}.profdata > cov_viz/cov.json
